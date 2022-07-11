5d71a30792bfbdda02b35bbdda621a0926b5ed8b
RenatiBhavana <bhavanarenati26@gmail.com>
2022-07-11 12:58:56 +0530

    new commit

---
+++ SwappingClass.java
@@ -0,0 +1,22 @@
+package com.swapping.example;
+
+public class SwappingClass {
+
+	public static void main(String[] args) {
+		int a= 10; //20   10 20 30  20 10 30   20 30 10
+		int b=20; //30
+	
+				
+		
+		System.out.println("Before swapping:" + a + " " + b);
+		
+		
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println("After swapping:" + a + " " + b);
+		
+
+	}
+
+}
