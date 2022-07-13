82851615955c225796f41026c31cbdceb3ce4c69
RenatiBhavana <bhavanarenati26@gmail.com>
2022-07-13 20:27:30 +0530

    maven project

---
+++ src/main/java/com/example/demo/ArrayList.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayList {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5);
+		
+	System.out.println(list);
+			
+			
+		
+	}
+
+}
